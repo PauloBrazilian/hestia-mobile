3027abd49feb58d737a8dafd1bf50bdf30cda1d2
Paulo Ricardo <ricardo.consul77@gmail.com>
2023-12-03 00:41:31 -0300

    deletando progresso para poder subir com outro com mapemento diferente e algumas logicas alteradas + add algumas bibliotecas

--- hestia/src/main/java/prjIntegrador/msPersons/repository/PersonBussRepository.java
+++
@@ -1,7 +0,0 @@
-package prjIntegrador.msPersons.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import prjIntegrador.msPersons.entity.PersonBUSS;
-
-public interface PersonBussRepository extends JpaRepository<PersonBUSS, Integer> {
-}
--- hestia/src/main/java/prjIntegrador/msPersons/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package prjIntegrador.msPersons.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import prjIntegrador.msPersons.entity.Person;
-
-public interface PersonRepository extends JpaRepository<Person, Integer> {
-}
